975f58f7dd1675b7054d5675f153b26f980d3631
wangyao <13564118478@163.com>
2019-07-10 16:14:02 +0800

    GG

--- handle_data/celery_config.py
+++ handle_data/celery_config.py
@@ -27,7 +27,7 @@ SURL = "mysql+pymysql://cic_admin:TaBoq,,1234@192.168.1.170:3306/yct_proxy?chars
 # REDIS_HOST = '192.168.20.245'
 # # REDIS_HOST = '192.168.1.152'
 # REDIS_PORT = 16379
-
+AA=1
 REDIS_HOST = 'aliyun_redis'
 # REDIS_HOST = '127.0.0.1'
 REDIS_PORT = 6379
